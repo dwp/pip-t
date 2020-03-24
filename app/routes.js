009423d905282fd03579ead298a6f65881e36992
Phil Swan <mail@philswan.co.uk>
2020-03-24 16:46:47 +0000

    Get GOV.UK content in

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Deal with query strings
+
+router.use(function(req, res, next){
+
+  Object.assign(res.locals,{
+    postData: (req.body ? req.body : false)
+  });
+
+  Object.assign(res.locals,{
+    getData: (req.query ? req.query : false)
+  });
+
+  next();
+});
+
 module.exports = router
