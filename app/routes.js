c242927f3e17b8de2622d8c56bc0d1ecf6ea6754
Phil Swan <mail@philswan.co.uk>
2020-03-30 18:01:38 +0100

    Lots of functional updates

--- app/routes.js
+++ app/routes.js
@@ -64,5 +64,18 @@ router.get('/v1/supporting-evidence-uploaded-3-data', function(req, res) {
     }
 });
 
+// Uploading screens
+
+router.get('/v1/uploading-supporting-evidence-1', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-1' });
+});
+
+router.get('/v1/uploading-supporting-evidence-2', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-2' });
+});
+
+router.get('/v1/uploading-supporting-evidence-3', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-3' });
+});
 
 module.exports = router
