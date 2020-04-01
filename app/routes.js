de6d410001f7ddce01062acf95c43f9f5c1d4955
Phil Swan <mail@philswan.co.uk>
2020-04-01 09:41:11 +0100

    Progress with CYA logic

--- app/routes.js
+++ app/routes.js
@@ -78,4 +78,111 @@ router.get('/v1/uploading-supporting-evidence-3', function(req, res) {
   res.render('v1/uploading', { 'file' : 'supporting-evidence-3' });
 });
 
+// Check you answers variations
+
+router.get('/v1/upload-cya', function(req, res) {
+  res.render('v1/upload', { 'cya' : 'true' });
+});
+
+router.get('/v1/uploading-cya', function(req, res) {
+  res.render('v1/uploading', { 'cya' : 'true' });
+});
+
+router.get('/v1/uploaded-1-cya', function(req, res) {
+  res.render('v1/uploaded-1', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-cya', function(req, res) {
+  res.render('v1/supporting-evidence', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-data-cya', function(req, res) {
+
+  let question = req.session.data['question']
+
+  if (question === 'yes') {
+      res.redirect('interruption-screen-cya')
+    } else {
+      res.redirect('check-your-answers')
+    }
+});
+
+router.get('/v1/interruption-screen-cya', function(req, res) {
+  res.render('v1/interruption-screen', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-upload-1-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-1', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-upload-2-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-2', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-upload-3-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-3', { 'cya' : 'true' });
+});
+
+router.get('/v1/uploading-supporting-evidence-1-cya', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-1', 'cya' : 'true' });
+});
+
+router.get('/v1/uploading-supporting-evidence-2-cya', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-2', 'cya' : 'true' });
+});
+
+router.get('/v1/uploading-supporting-evidence-3-cya', function(req, res) {
+  res.render('v1/uploading', { 'file' : 'supporting-evidence-3', 'cya' : 'true' });
+});
+
+
+router.get('/v1/supporting-evidence-upload-1-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-1', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-upload-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-2', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-upload-3-cya', function(req, res) {
+  res.render('v1/supporting-evidence-upload-3', { 'cya' : 'true' });
+});
+
+router.get('/v1/supporting-evidence-uploaded-1-data-cya', function(req, res) {
+
+  let question = req.session.data['question']
+
+  if (question === 'yes') {
+      res.redirect('supporting-evidence-upload-2-cya')
+    } else {
+      res.redirect('v1/check-your-answers')
+    }
+});
+
+router.get('/v1/supporting-evidence-uploaded-2-data-cya', function(req, res) {
+
+  let question = req.session.data['question']
+
+  if (question === 'yes') {
+      res.redirect('supporting-evidence-upload-3-cya')
+    } else {
+      res.redirect('v1/check-your-answers')
+    }
+});
+
+router.get('/v1/supporting-evidence-uploaded-3-data-cya', function(req, res) {
+
+  let question = req.session.data['question']
+
+  if (question === 'yes') {
+      res.redirect('supporting-evidence-upload-4-cya')
+    } else {
+      res.redirect('v1/check-your-answers')
+    }
+});
+
+
+
+
+
 module.exports = router
